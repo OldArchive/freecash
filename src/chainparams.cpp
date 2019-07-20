322d67c88f60530d885b2510459587950eafb6c1
caisirius <caisirius@sina.com>
2019-07-20 09:46:44 +0800

    feat: set maturity height to 100

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -236,8 +236,8 @@ public:
 
         consensus.rewardAddress = "FGwx11b5jnXkfpTzehuMFdCVnnqc1pCphH";
         vRewardAddresses.push_back(consensus.rewardAddress);
-        consensus.coinbaseMaturity = 14400;
-        consensus.developerRewardMaturity = 144000;
+        consensus.coinbaseMaturity = 100;
+        consensus.developerRewardMaturity = 200;
     }
 };
 
@@ -328,8 +328,8 @@ public:
 
         consensus.rewardAddress = "mrdnqGEyhVkLpm4aWbDa79tHd8RJ7rvp5g";
         vRewardAddresses.push_back(consensus.rewardAddress);
-        consensus.coinbaseMaturity = 14400;
-        consensus.developerRewardMaturity = 144000;
+        consensus.coinbaseMaturity = 100;
+        consensus.developerRewardMaturity = 200;
     }
 };
 
